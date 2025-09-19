f95740e24b08cc3f763fba8dce126aa6eafd8f17
Giancarlo Ortega <giancarloortega065@gmail.com>
2025-09-19 11:34:24 -0500

    Update vectorstore.py

--- src/vectorstore.py
+++ src/vectorstore.py
@@ -3,6 +3,8 @@ vectorstore.py
 Simple wrapper around FAISS for storing and retrieving embeddings + metadata.
 """
 
+# src/vectorstore.py
+
 import faiss
 import numpy as np
 import pickle
@@ -15,15 +17,18 @@ class FaissStore:
         self.index_path = index_path
 
     def add(self, embeddings, meta_list):
+        """Add embeddings + metadata"""
         emb = np.vstack(embeddings).astype("float32")
         self.index.add(emb)
         self.metadata.extend(meta_list)
 
-    def search(self, query_emb, k=5):
+    def search(self, query_emb, k=3):
+        """Return top-k closest metadata entries"""
         D, I = self.index.search(query_emb.astype("float32"), k)
         results = []
         for idx in I[0]:
-            results.append(self.metadata[idx])
+            if idx < len(self.metadata):
+                results.append(self.metadata[idx])
         return results
 
     def save(self):
@@ -37,3 +42,4 @@ class FaissStore:
             self.index = faiss.read_index(self.index_path + ".index")
             with open(self.index_path + ".meta", "rb") as f:
                 self.metadata = pickle.load(f)
+
